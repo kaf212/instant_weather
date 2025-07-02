6bdb2e33f7b1c4bcd0bbea4274c6ae3474c0f5fb
kaf212 <kaf221122@gmail.com>
2025-07-02 16:26:16 +0200

    Add missing files

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:instant_weather/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final locator = Locator();
+  Position? currentCoordinates;
+  String? currentPlace;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationStream();
+  }
+
+  void getLocationStream() {
+    locator.getPositionStream().then((stream) {
+    stream.listen((Position fetchedPosition) {
+      setState(() {
+        print(fetchedPosition);
+        currentCoordinates = fetchedPosition;
+        getNameOfPlaceByCoordinates(currentCoordinates);
+      });
+    });
+  }).catchError((e) {
+    print('Error: $e');
+  });
+  }
+
+  void getNameOfPlaceByCoordinates(coordinates) {
+    locator.getPlaceMark(currentCoordinates?.latitude, currentCoordinates?.longitude).then((placemarks) {
+      setState(() {
+        final locality = placemarks.first.locality;
+        final name = placemarks.first.name;
+        //print(placemarks.first);
+        if (locality != "") {
+          currentPlace = locality;
+        } else {
+          currentPlace = name;
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$currentPlace',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+

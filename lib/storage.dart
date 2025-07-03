da9b25fd872622db6541697deec9f3446f5a9671
kaf212 <kaf221122@gmail.com>
2025-07-03 16:40:20 +0200

    Put storage functions in a class

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,12 +1,14 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> writeData(String key, String value) async {
-  final sharedPreferences = await SharedPreferences.getInstance();
+class PersistentStorage {
+  Future<void> writeData(String key, String value) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
 
-  await sharedPreferences.setString(key, value);
-}
+    await sharedPreferences.setString(key, value);
+  }
 
-Future<String?> readData(String key) async {
-  final sharedPreferences = await SharedPreferences.getInstance();
-  return sharedPreferences.getString(key);
+  Future<String?> readData(String key) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getString(key);
+  }
 }
